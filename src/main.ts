2b93f42861f8fb088d58bc8118b893ee4de6c222
Francis M <francis.macatangay@netzontech.com>
2021-05-07 19:27:17 +0800

    chore: persist to storage akita state

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { persistState } from '@datorama/akita';
+
+const storage = persistState({ key: 'blue-ionic' });
+
+const providers = [{ provide: 'persistStorage', useValue: storage }];
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic(providers)
+  .bootstrapModule(AppModule)
   .catch(err => console.log(err));
