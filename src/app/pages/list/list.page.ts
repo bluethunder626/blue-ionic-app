9747c3b12aeb3885d0dc826fb886e29ce21ec62e
Francis M <francis.macatangay@netzontech.com>
2021-05-08 14:42:35 +0800

    chore: list page implementation

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RecordsQuery, RecordsService, IRecordStore } from './../../states/records';
+import { generateIRecordStore } from './../add/add.util';
 
 @Component({
   selector: 'app-list',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.page.scss'],
 })
 export class ListPage implements OnInit {
+  public records$: Observable<IRecordStore[]>;
 
-  constructor() { }
+  constructor(
+    private recordsQuery: RecordsQuery,
+    private recordsService: RecordsService,
+  ) { }
 
   ngOnInit() {
+    this.records$ = this.recordsQuery.selectAll();
   }
 
+  onClickRecord(id: string) {
+    console.log(id);
+  }
+
+  onClickAddMulti() {
+    // add 10000 records
+    generateIRecordStore(10000).then(r => this.recordsService.addRecords(r));
+  }
 }
