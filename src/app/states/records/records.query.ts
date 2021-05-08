af6bcbf9d9a9d5cf1e53287d10b5bd350cae7580
Francis M <francis.macatangay@netzontech.com>
2021-05-08 18:43:46 +0800

    chore: add query for total and biggest amount

--- src/app/states/records/records.query.ts
+++ src/app/states/records/records.query.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Order, QueryConfig, QueryEntity } from '@datorama/akita';
+import { map } from 'rxjs/operators';
+import { isThisMonth } from 'date-fns';
 import { RecordsStore, RecordsState } from './records.store';
 
 @Injectable({ providedIn: 'root' })
@@ -8,6 +10,25 @@ import { RecordsStore, RecordsState } from './records.store';
   sortByOrder: Order.ASC
 })
 export class RecordsQuery extends QueryEntity<RecordsState> {
+  getTotalThisMonth$
+    = this.selectAll({
+      filterBy: r => isThisMonth(new Date(r.datetime))
+    }).pipe(
+      map(records => {
+        let total = 0;
+        records.forEach(r => total += r.amount);
+        return total;
+      })
+    );
+
+  getBiggestThisMonth$
+    = this.selectAll({
+      filterBy: r => isThisMonth(new Date(r.datetime)),
+      sortBy: 'amount',
+      sortByOrder: Order.DESC,
+    }).pipe(
+      map(r => r.length > 0 ? r[0].amount : 0)
+    );
 
   constructor(protected store: RecordsStore) {
     super(store);
